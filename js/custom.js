bd1e403b490bdb7974a41ee509823807d4361e51
Will Carhart <wcarhart@sandiego.edu>
2019-12-15 22:52:31 -0800

    Added social media links + formatting, just need to fix mobile format + background animation

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,3 @@
-/*========== NAVBAR TRANSPARENT TO SOLID ==========*/
-
-$(document).ready(function() { //when document(DOM) loads completely. 
-        // Transition effect for navbar 
-        $(window).scroll(function() { //function is called while you scrolls 
-          // checks if window is scrolled more than 300px, adds/removes solid class
-          if($(this).scrollTop() > 300) { 
-              $('.navbar').addClass('solid'); //add class 'solid' in any element which has class 'navbar'
-          } else {
-              $('.navbar').removeClass('solid'); //remove class 'solid' in any element which has class 'navbar'
-          }
-        });
-});
-
-/*========== CLOSE MOBILE NAV ON CLICK ==========*/
-
-$(document).ready(function () { //when document loads completely.
-    $(document).click(function (event) { //click anywhere
-        var clickover = $(event.target); //get the target element where you clicked
-        var _opened = $(".navbar-collapse").hasClass("show"); //check if element with 'navbar-collapse' class has a class called show. Returns true and false.
-        if (_opened === true && !clickover.hasClass("navbar-toggler")) { // if _opened is true and clickover(element we clicked) doesn't have 'navbar-toggler' class
-            $(".navbar-toggler").click(); //toggle the navbar; close the navbar menu in mobile.
-        }
-    });
-});
-
 /*========== SMOOTH SCROLLING TO LINKS ==========*/
 
 $(document).ready(function(){ //document is loaded
@@ -50,27 +24,6 @@ $(document).ready(function(){ //document is loaded
         //Optional remove "window.location.hash = hash;" to prevent transparent navbar on load
       });
     } // End if
-  });
-});
-
-/*========== BOUNCING DOWN ARROW ==========*/
-//down arrow at top
-$(document).ready(function(){
-$(window).scroll(function(){ //browser scroll 
-    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); //set opacity css from 1 to -(negative) infinity of element with class 'arrow'
-  //250 is fade pixels
-  });
-});
-
-/*========== TOP SCROLL BUTTON ==========*/
-
-$(document).ready(function() { //when document is ready
-  $(window).scroll(function() { //when webpage is scrolled
-    if ($(this).scrollTop() > 500) { //if scroll from top is more than 500
-      $('.top-scroll').fadeIn(); //display element with class 'top-scroll'; opacity increases
-    } else { //if not
-      $('.top-scroll').fadeOut(); //hide element with class 'top-scroll'; opacity decreases
-    }
   });
 });
 
